a49425de5e0207de9fabc36d1c421f6b407fe9b1
Baron Chen <baron.zhongyangchen@gmail.com>
2018-03-25 21:19:06 +1100

    Fix unit tests

--- src/app/converter/number-input/components/number-input-card/number-input-card.component.spec.ts
+++ src/app/converter/number-input/components/number-input-card/number-input-card.component.spec.ts
@@ -1,17 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumberInputCardComponent } from './number-input-card.component';
-import { Store, StoreModule } from '@ngrx/store';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('NumberInputCardComponent', () => {
   let component: NumberInputCardComponent;
   let fixture: ComponentFixture<NumberInputCardComponent>;
-  let store: Store<any>;
 
   beforeEach(async() => {
     TestBed.configureTestingModule({
-      imports: [ StoreModule.forRoot({}) ],
       declarations: [ NumberInputCardComponent ],
       schemas: [ NO_ERRORS_SCHEMA ]
     });
@@ -22,9 +19,7 @@ describe('NumberInputCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NumberInputCardComponent);
     component = fixture.componentInstance;
-    store = TestBed.get(Store);
-
-    spyOn(store, 'dispatch').and.callThrough();
+   
     fixture.detectChanges();
   });
 
--- src/app/converter/number-input/number-input.component.spec.ts
+++ src/app/converter/number-input/number-input.component.spec.ts
@@ -3,12 +3,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NumberInputComponent } from './number-input.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { Store, StoreModule } from '@ngrx/store';
+
+
 describe('NumberInputComponent', () => {
   let component: NumberInputComponent;
   let fixture: ComponentFixture<NumberInputComponent>;
-
+  let store: Store<any>;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ StoreModule.forRoot({}) ],
       declarations: [ NumberInputComponent ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -18,6 +23,9 @@ describe('NumberInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NumberInputComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
