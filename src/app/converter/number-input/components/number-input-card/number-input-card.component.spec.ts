73f1675b4f15b9e1ad096f09af1422245747f30e
Baron Chen <baron.zhongyangchen@gmail.com>
2018-04-20 22:08:05 +1000

    Add tests for number-input-form

--- src/app/converter/number-input/components/number-input-card/number-input-card.component.spec.ts
+++ src/app/converter/number-input/components/number-input-card/number-input-card.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumberInputCardComponent } from './number-input-card.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, ChangeDetectionStrategy, DebugElement } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { By } from '@angular/platform-browser';
 
 describe('NumberInputCardComponent', () => {
   let component: NumberInputCardComponent;
   let fixture: ComponentFixture<NumberInputCardComponent>;
+  let testError = 'testError';
+  let testNumber = '12345';
 
   beforeEach(async() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
       declarations: [ NumberInputCardComponent ],
       schemas: [ NO_ERRORS_SCHEMA ]
+    }).overrideComponent(NumberInputCardComponent, {
+      set: {  changeDetection: ChangeDetectionStrategy.Default  }
     });
 
     await TestBed.compileComponents();
@@ -19,11 +27,34 @@ describe('NumberInputCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NumberInputCardComponent);
     component = fixture.componentInstance;
-   
+    component.error = testError;
+    component.number = testNumber;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render base on inputs', () => {
+    let numberEl = fixture.debugElement.query(By.css('input'));
+    let errorEl = fixture.debugElement.query(By.css('mat-error'));
+
+    expect(numberEl.nativeElement.value).toEqual(testNumber);
+    expect(errorEl.nativeElement.textContent.trim()).toEqual(testError);
+  });
+
+  it('should fire event when input changes', () => {
+    spyOn(component.numberChange, 'emit').and.callFake(() => {});
+    const newTestNumber = '56789';
+    let numberEl = fixture.debugElement.query(By.css('input'));
+    numberEl.nativeElement.value = newTestNumber;
+    numberEl.nativeElement.dispatchEvent(new Event('input'));
+    numberEl.nativeElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.numberChange.emit).toHaveBeenCalledWith(newTestNumber);
+    })
+  });
+
 });
--- src/app/converter/number-input/number-input.component.spec.ts
+++ src/app/converter/number-input/number-input.component.spec.ts
@@ -1,29 +1,59 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumberInputComponent } from './number-input.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Input, Output, EventEmitter, Directive } from '@angular/core';
 
 import { Store, StoreModule, INITIAL_STATE } from '@ngrx/store';
 
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { By } from '@angular/platform-browser';
+import * as fromStore from '../reducers';
+import * as actions from './actions/number-input.actions';
+
+@Directive({
+  selector: 'app-number-input-card'
+})
+class MockNumberInputCard {
+  @Input()
+  number: string;
+
+  @Input()
+  error: string;
+
+  @Output()
+  numberChange: EventEmitter<any> = new EventEmitter();
+}
 
 describe('NumberInputComponent', () => {
   let component: NumberInputComponent;
   let fixture: ComponentFixture<NumberInputComponent>;
-  let store: Store<any>;
-  
+  let mockStore: any;
+  let mockNumberSubject: ReplaySubject<any>;
+  let mockErrorSubject: ReplaySubject<any>;
+  let testNumber = '12345';
+  let testError = 'test_error';
+ 
   beforeEach(async(() => {
+    mockNumberSubject = new ReplaySubject(10);
+    mockErrorSubject = new ReplaySubject(10);
+    mockStore = {
+      select: (selector) => {
+        let testState = {
+          number: mockNumberSubject,
+          error: mockErrorSubject 
+        }
+        return selector.projector(testState);
+      },
+      dispatch: () => {}
+    }
+
+    spyOn(mockStore, 'select').and.callThrough();
+    spyOn(mockStore, 'dispatch').and.callThrough();
     TestBed.configureTestingModule({
       imports: [ StoreModule.forRoot({}) ],
-      declarations: [ NumberInputComponent ],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [{provide: INITIAL_STATE, useValue: {
-        converter: {
-          numberInput: {
-            number: '',
-            error: ''
-          }
-        }
-      }}]
+      declarations: [ NumberInputComponent, MockNumberInputCard ],
+      providers: [{provide: Store, useFactory: () => mockStore}]
     })
     .compileComponents();
   }));
@@ -31,12 +61,36 @@ describe('NumberInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NumberInputComponent);
     component = fixture.componentInstance;
-    store = TestBed.get(Store);
-    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should pass value to child', () => {
+
+    expect(mockStore.select).toHaveBeenCalledWith(fromStore.getNumber);
+    expect(mockStore.select).toHaveBeenCalledWith(fromStore.getNumberInputError);
+    mockNumberSubject.next(testNumber);
+    mockErrorSubject.next(testError);
+    fixture.detectChanges();
+
+    let mockChildComponentEl = fixture.debugElement.query(By.directive(MockNumberInputCard));
+    let mockNumberInpurCard = mockChildComponentEl.injector.get(MockNumberInputCard) as MockNumberInputCard;
+
+    expect(mockNumberInpurCard.error).toEqual(testError);
+    expect(mockNumberInpurCard.number).toEqual(testNumber);
+
+  });
+
+  it('Should emit event to parent', () => {
+    
+    let mockChildComponentEl = fixture.debugElement.query(By.directive(MockNumberInputCard));
+    let mockNumberInpurCard = mockChildComponentEl.injector.get(MockNumberInputCard) as MockNumberInputCard;
+    
+    mockNumberInpurCard.numberChange.emit(testNumber);
+    expect(mockStore.dispatch).toHaveBeenCalledWith(new actions.UpdateNumber(testNumber));
+  });
+
 });
